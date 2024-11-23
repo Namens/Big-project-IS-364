28ffe299267bdc0b0c87c98a6fe4e2e0151c6dad
Namens <smackovdaniil@mail.ru>
2024-11-23 19:01:07 +0300

    second commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Регистрация</title>
+    <link rel="stylesheet" href="access/css/style.css">
+</head>
+<body>
+    
+    <main class="main">
+
+        <form method="POST" class="card">
+            
+            <label for="name">Имя</label>
+            <input type="text" name="name" class="inp" required><br>
+            
+            <label for="email">Email</label>
+            <input type="email" name="email" class="inp" required><br>
+            
+            <label for="password">Пароль</label>
+            <input type="password" name="password" class="inp" required><br>
+
+        </form>
+
+    </main>
+
+</body>
+</html>
